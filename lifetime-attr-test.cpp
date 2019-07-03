fd133f56f66ad30cec99e5d36e9141f2a4056455
Matthias Gehre <gehre.matthias@gmail.com>
2019-07-04 00:38:51 +0200

    lifetime-attr-test.cpp: more tests

--- lifetime-attr-test.cpp
+++ lifetime-attr-test.cpp
@@ -1,5 +1,20 @@
 #include <vector>
-
 static_assert(__is_gsl_owner(std::vector<int>), "");
 static_assert(__is_gsl_pointer(std::vector<int>::iterator), "");
 static_assert(__is_gsl_pointer(std::vector<int>::const_iterator), "");
+
+#include <string>
+static_assert(__is_gsl_owner(std::string), "");
+static_assert(__is_gsl_pointer(std::string::iterator), "");
+static_assert(__is_gsl_pointer(std::string::const_iterator), "");
+
+#if __has_include(<string_view>)
+#include <string_view>
+static_assert(__is_gsl_pointer(std::string_view), "");
+static_assert(__is_gsl_pointer(std::string_view::iterator), "");
+static_assert(__is_gsl_pointer(std::string_view::const_iterator), "");
+
+static_assert(__is_gsl_pointer(std::wstring_view), "");
+static_assert(__is_gsl_pointer(std::wstring_view::iterator), "");
+static_assert(__is_gsl_pointer(std::wstring_view::const_iterator), "");
+#endif
