6c284c64dddce84037f560596c74ec67439a235c
Matthias Gehre <gehre.matthias@gmail.com>
2019-07-09 23:11:27 +0200

    More tests an improve script

--- lifetime-attr-test.cpp
+++ lifetime-attr-test.cpp
@@ -2,11 +2,30 @@
 static_assert(__is_gsl_owner(std::vector<int>), "");
 static_assert(__is_gsl_pointer(std::vector<int>::iterator), "");
 static_assert(__is_gsl_pointer(std::vector<int>::const_iterator), "");
+static_assert(__is_gsl_pointer(std::vector<int>::reverse_iterator), "");
+static_assert(__is_gsl_pointer(std::vector<int>::const_reverse_iterator), "");
 
 #include <string>
 static_assert(__is_gsl_owner(std::string), "");
 static_assert(__is_gsl_pointer(std::string::iterator), "");
 static_assert(__is_gsl_pointer(std::string::const_iterator), "");
+static_assert(__is_gsl_pointer(std::string::reverse_iterator), "");
+static_assert(__is_gsl_pointer(std::string::const_reverse_iterator), "");
+
+#if __has_include(<optional>)
+#include <optional>
+static_assert(__is_gsl_owner(std::optional<int>), "");
+#endif
+
+#if __has_include(<any>)
+#include <any>
+static_assert(__is_gsl_owner(std::any), "");
+#endif
+
+#if __has_include(<variant>)
+#include <variant>
+static_assert(__is_gsl_owner(std::variant<int, char>), "");
+#endif
 
 #if __has_include(<string_view>)
 #include <string_view>
